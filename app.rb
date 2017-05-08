5a543a8d51e1d8bf852d79b63f141f7ebe8bc3f2
Ilene Gorski and Max Reichman <Guest@epicodus-10.local>
2017-05-08 10:22:29 -0700

    Make it work good.

---
+++ app.rb
@@ -0,0 +1,68 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("sinatra/activerecord")
+require("./lib/task")
+require("./lib/list")
+require("pg")
+
+get("/") do
+  @lists = List.all()
+  @tasks = Task.all()
+  erb(:index)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  @lists = List.all()
+  @tasks = Task.all()
+  erb(:index)
+end
+
+get("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list)
+end
+
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  task = Task.new({:description => description, :list_id => list_id})
+  task.save()
+  @list = List.find(list_id)
+  erb(:list)
+end
+
+get("/lists/:id/edit") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list_edit)
+end
+
+patch("/lists/:id") do
+  name = params.fetch("name")
+  @list = List.find(params.fetch("id").to_i())
+  @list.update({:name => name})
+  erb(:list)
+end
+
+delete("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  @list.delete()
+  @lists = List.all()
+  erb(:index)
+end
+
+get('/tasks/:id/edit') do
+  @task = Task.find(params.fetch("id").to_i())
+  erb(:task_edit)
+end
+
+patch("/tasks/:id") do
+  description = params.fetch("description")
+  @task = Task.find(params.fetch("id").to_i())
+  @task.update({:description => description})
+  @tasks = Task.all()
+  erb(:index)
+end
---
+++ lib/task.rb
@@ -0,0 +1,8 @@
+class Task < ActiveRecord::Base
+  belongs_to(:list)
+  scope(:not_done, -> do
+    where({:done => false})
+  end)
+
+
+end
